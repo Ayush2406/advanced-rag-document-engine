bfd47dbdcdbecafa6ec6620d27caa193bfdfae22
Ayush <dayush0524@gmail.com>
2026-08-15 22:53:45 +0530

    feat: Embedding manager

---
+++ src/retrieval/embedding_manager.py
@@ -0,0 +1,20 @@
+import torch
+from src.config import settings
+from langchain_huggingface import HuggingFaceEmbeddings
+
+def get_embeddings_model() -> HuggingFaceEmbeddings:
+    
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
+        
+    embeddings = HuggingFaceEmbeddings(
+        model_name = settings.EMBEDDING_MODEL_NAME,
+        model_kwags={"device":device},
+        encode_kwargs = {"normalize_embeddings":True}
+    )
+    
+    return embeddings
